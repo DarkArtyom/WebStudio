ce5a5af458fab7a39f9d297d204481ceb37bd1a5
Artem Shnurko <shnurko.artyom@gmail.com>
2022-05-22 20:12:01 +0200

    studio adapt

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector(".mobile__button"),
     closeMenuBtn: document.querySelector(".menu-btn__close"),
     menu: document.querySelector(".mobile-menu"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
